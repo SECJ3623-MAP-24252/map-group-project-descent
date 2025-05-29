a813a65b20715ec36213dc9619297e636d0aae9f
WaleedOthman2525 <alkhair20@graduate.utm.my>
2025-05-30 02:23:21 +0800

    View Entry/Food Detail complete

--- lib/pages/forget.dart
+++ lib/pages/forget.dart
@@ -1,25 +1,12 @@
-<<<<<<< HEAD
-// forgot_password_screen.dart
-import 'package:flutter/material.dart';
-=======
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
->>>>>>> 1c92d99cd81646824f609e845f6fd8677a4b0bc1
 
 class ForgetPage extends StatelessWidget {
   const ForgetPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return Scaffold(
-      appBar: AppBar(title: Text("Reset Password")),
-=======
     final emailController = TextEditingController();
-
-    Future<void> _resetPassword() async {
-      try {
-        await FirebaseAuth.instance.sendPasswordResetEmail(
           email: emailController.text.trim(),
         );
         ScaffoldMessenger.of(context).showSnackBar(
@@ -37,12 +24,10 @@ class ForgetPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: const Text("Reset Password")),
->>>>>>> 1c92d99cd81646824f609e845f6fd8677a4b0bc1
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-<<<<<<< HEAD
             Text("Enter your email to reset password"),
             SizedBox(height: 10),
             TextField(
@@ -52,7 +37,7 @@ class ForgetPage extends StatelessWidget {
             ElevatedButton(
               onPressed: () {},
               child: Text("Send Reset Link"),
-=======
+
             const Text("Enter your email to reset password"),
             const SizedBox(height: 10),
             TextField(
@@ -64,15 +49,11 @@ class ForgetPage extends StatelessWidget {
             ElevatedButton(
               onPressed: _resetPassword,
               child: const Text("Send Reset Link"),
->>>>>>> 1c92d99cd81646824f609e845f6fd8677a4b0bc1
+
             ),
           ],
         ),
       ),
     );
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 1c92d99cd81646824f609e845f6fd8677a4b0bc1

a6263001d43971b0ed64a40cf586319e1423d9b3
WaleedOthman2525 <alkhair20@graduate.utm.my>
2025-06-13 04:28:21 +0800

    Working camera scanner

--- lib/pages/forget.dart
+++ lib/pages/forget.dart
@@ -1,5 +1,5 @@
-// import 'package:flutter/material.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ForgetPage extends StatefulWidget {
   const ForgetPage({super.key});
